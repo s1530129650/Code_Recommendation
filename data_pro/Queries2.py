4e1b1e3b7fef0a4e7728b09f96e1692ae1355aec
v-enshi <1530129650@qq.com>
2019-04-23 11:28:51 +0800

    add data processsing

--- data_pro/Queries2.py
+++ data_pro/Queries2.py
@@ -39,11 +39,7 @@ def data_loading(filepath):
     for i in range(len(data1)):
         content = json.loads(data1[i])
         data.append(content)
-    '''
-    with open(filepath, 'r') as load_f:
-        data1 = load_f.readlines()
-        data = json.loads(data1[0])  # 这里之后替换为上面的
-    '''
+
     return data
 
 
@@ -78,6 +74,7 @@ type_vocab,value_vocab = build_vocab(training_data)
 
 
 # 3. make the queries
+# 3. make the queries
 def Queries(data):
     data_rd = []
     random = np.random.RandomState(0)
@@ -85,25 +82,78 @@ def Queries(data):
         length = len(data[i])
         if length <= CONTEXT_WINDOW + 1:
             continue
-        rd = random.randint(CONTEXT_WINDOW + 1, len(data[i]) - 1)
+        rd = random.randint(CONTEXT_WINDOW, length - 1)
         while "value" not in data[i][rd].keys():  # 1.look for leaf node
             rd = rd + 1
-        for j in range(rd - 1, rd - CONTEXT_WINDOW,
+            if rd >= length:
+                break
+        if rd >= length:
+            continue
+
+        query = []
+        # find same node in the context
+        for j in range(rd - 1, rd - CONTEXT_WINDOW - 1,
                        -1):  # whether the remove node in the context.if the node in context,we remeber the position in context
-            if j < 0:
-                break  # some sequence length is less than CONTEXT_WINDOW
+
             if data[i][rd]["type"] == data[i][j]["type"] and "value" in data[i][j].keys() and data[i][rd]["value"] == \
                     data[i][j]["value"]:
-                query = [data[i][:rd], [data[i][rd]], rd - j - 1, rd]  # 和前面某个node一样，node的序号是0...context_window - 1
-                data_rd.append(query)
-
+                #print("j$$$$$$$$$$$",rd - 1, rd - CONTEXT_WINDOW - 1,j,rd - j - 1)
+                query = [data[i][:rd], [data[i][rd]], rd, rd - j - 1]
                 break
+        if j == rd - CONTEXT_WINDOW:  # there is no same node in context
+            continue
+        # add parents node
+        for j in range(rd - 1, rd - CONTEXT_WINDOW - 1, -1):
+
+            if "children" in data[i][j].keys() and rd in data[i][j]["children"]:
+                query.append(rd - j - 1)
+                break
+            if j == rd - CONTEXT_WINDOW:
+                query.append(CONTEXT_WINDOW - 1)
+                break
+        # query = [context,predict_node,position, same_node_position,parent_node_position]
+        data_rd.append(query)
     return data_rd
 training_queries = Queries(training_data)
+#print(training_queries)
+
+
+
+#4. text -> idex
+def prepare_sequence(seq, val_to_ix, type_to_ix):  # trans code to idex
+    idxs_ty = []
+    idxs_vl = []
+    for node in seq:
+        if "value" in node.keys():
+            if node["value"] in val_to_ix.keys():
+                idxs_vl.append(val_to_ix[ node["value"] ])
+            else:
+                idxs_vl.append(val_to_ix["UNK"])
+        else:
+            idxs_vl.append(val_to_ix["UNK"])
+        idxs_ty.append(type_to_ix[node["type"]])
+    return torch.tensor([idxs_ty, idxs_vl], dtype=torch.long, device=device)
+
+input = np.array([])
+parent = np.array([])
+target = np.array([])
+
+
+
+for i in range(len(training_queries)):
+    sequence = training_queries[i][0]
+    input  = np.append(input, prepare_sequence(sequence, value_vocab, type_vocab))
+    parent = np.append(parent,training_queries[i][4])
+    target = np.append(target,torch.tensor([training_queries[i][3]], dtype=torch.long, device=device))
+
+
+np.savez('array_save.npz',input_data = input ,parent_data = parent ,target_data = )
+
 """
+
 with codecs.open(r"..\data\python\QUERIES\python_train.json",'w', 'utf-8') as outf:
     for items in training_queries:
         json.dump(items, outf, ensure_ascii=False)
-        outf.write('\n')"""
+        outf.write('\n')
 
-
+"""
