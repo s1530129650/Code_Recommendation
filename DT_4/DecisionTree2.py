3ca59121611daf0b710e7102213a700e729880e6
Enshengshi <34572118+s1530129650@users.noreply.github.com>
2019-05-23 09:33:47 +0800

    add DT_4

---
+++ DT_4/DecisionTree2.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+"""
+@version: python3.7
+@author: v-enshi
+@license: Apache Licence 
+@contact: 123@qq.com
+@site: 
+@software: PyCharm
+@file: DecisionTree2.py
+@time: 2019/5/13 18:57
+position information
+"""
+
+from gene_queries_occur1 import gene_queries
+from Tgen1 import TGEN
+from collections import Counter
+from probablistic_model1 import  traverse2model
+import numpy as np
+from GA_1 import GA_Algorithm
+#from GA_ACCe import GA_Algorithm
+
+from math import log
+import random
+import sys
+import copy
+import time
+sys.setrecursionlimit(150000)  # set the maximum depth as 150
+start_time = time.time()
+
+#print("len(quer_data)",len(quer_data))
+
+
+#2 calulate entropy
+def cal_entropy(data_cal):
+    label_list =  []
+    numEntries = len(data_cal)
+
+    for i in range(numEntries):
+        label_list.append(data_cal[i].label)
+    labelCounts = Counter(label_list )
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+#entropy = cal_entropy(quer_data)
+#print("entorpy",entropy)
+
+# 3 generate feature
+def gene_feature(mv_instr,write_instr,feature_num):
+    import itertools
+    instr = []
+    for wr in write_instr:
+        instr.append(wr)
+    for i in range(1, feature_num):
+        iter1 = itertools.permutations(mv_instr, i)
+        list_iter = list(iter1)
+        for wr in write_instr:
+            iter2 = [list(c) + wr for c in list_iter]
+            instr= instr + list(iter2)
+    return instr
+
+
+# 4 split data
+def split_set(universalSet, item):
+    retDataSet = []
+    i = 0
+    #print("item",item)
+    while i < (len(universalSet)):
+        if universalSet[i].ctx[-1] == item:  #
+
+            retDataSet.append(universalSet[i])
+            universalSet.remove(universalSet[i])
+            i = i - 1
+        i = i + 1
+    # print("split",len(retDataSet))
+    #print("dataSet_ch", len(universalSet))
+    return retDataSet
+
+
+## 5 max gain ratio
+def choose_feature_split(query_data,instr):
+    fea_list = []
+    base_entropy = cal_entropy(query_data)  # dataset entropy
+
+    best_idx = GA_Algorithm(base_entropy ,query_data,instr)
+    bestFeature = instr[best_idx]
+
+    return best_idx,bestFeature
+
+#best_feature = choose_feature_split(quer_data,instructions,write_instruction)
+
+## 6 creat tree
+length = [0]
+def create_tree(all_data,instruc):
+
+    label = []
+    data_len = len(all_data)
+    for data in all_data:
+        data.resetNode()
+        label.append(data.label)
+    label_list = Counter(label)  # all_data label
+    if data_len < MIN_SIZE:  # too smaller
+        length[0] += 1
+        return all_data
+    if not instruc:
+        length[0] += 1
+        return all_data
+    print("start choose_feature_split")
+    now1 = time.time()
+    idx,p_best_feat = choose_feature_split(all_data, instruc)
+    now2 = time.time()
+    print("choose_feature_split",(now2- now1)/60)
+
+    if not p_best_feat:
+        length[0] += 1
+        return all_data
+    print( p_best_feat,)
+    bestFeatLabel = tuple(p_best_feat)
+    myTree = {bestFeatLabel: {}}
+
+    fea_list = []
+    len_list = []
+    for data in all_data:
+        data.geneProgram(p_best_feat)
+        fea_list.append(data.ctx[-1])
+        len_list.append(data.length)
+        #print(data.tree[-5:])
+    uniqueCtx = Counter(fea_list)  # datasize feature
+    uniqueCtx = uniqueCtx.most_common(10)
+    '''
+    for i in range(len(uniqueCtx )):
+        # print( uniqueCtx_ch[i] )
+        if uniqueCtx [i][1] < int(MIN_SIZE/2):
+            del uniqueCtx[i:]
+            break
+'''
+
+    print(uniqueCtx)
+    #print(len_list)
+    for last_ctx in uniqueCtx:
+        subLabels = copy.deepcopy(instruc)
+        del subLabels[idx]
+        myTree[bestFeatLabel][last_ctx[0]] = create_tree(split_set(all_data, last_ctx[0]), subLabels)
+    if len(all_data) > 0:
+        subLabels = copy.deepcopy(instruc)
+        del subLabels[idx]
+        myTree[bestFeatLabel]["DEFAULT:"] = create_tree(all_data, subLabels)
+
+
+    return myTree
+
+#myTree = create_tree(quer_data,instructions,write_instruction)
+
+
+# 7. code suggestion
+def code_suggestion(inputTree, test_data):  # single test_data
+    firstStr = list(inputTree.keys())[0]
+    secondDict = inputTree[firstStr]
+    test_data.geneProgram(firstStr)
+    test_data.resetNode()
+    suggestion = []
+    flag =  False
+    for key in secondDict.keys():  # secondDict.keys()＝[0, 1, 2]
+        if test_data.ctx[-1] == key:  # secondDict[key]＝N
+            flag = True
+            if type(secondDict[key]).__name__ == 'dict':  # type(secondDict[key]).__name__＝str
+                suggestion = code_suggestion(secondDict[key], test_data)
+            else:
+                suggestion = secondDict[key]
+    if not flag and "DEFAULT:" in secondDict.keys():
+        key = "DEFAULT:"
+        if type(secondDict[key]).__name__ == 'dict':  # type(secondDict[key]).__name__＝str
+            suggestion = code_suggestion(secondDict[key], test_data)
+        else:
+            suggestion = secondDict[key]
+
+
+    return suggestion
+# tree = copy.deepcopy(myTree)
+# tree1 = traverse2model(tree)
+
+#7 . evaluation
+def eval(model,eval_data):
+    length = len(eval_data)
+    #AP
+    def AP(pre, ground_true):
+        Ap = 0
+        for i in range(len(pre)):
+            if pre[i][0] == ground_true:
+                Ap = Ap + 1 / (i + 1)
+        return Ap
+
+    A_P = []
+    for data in eval_data:
+        #start = time.time()
+        res = code_suggestion(model,data)
+        if not res:
+            length = length - 1
+            continue
+
+        result = sorted(res[0].items(), key=lambda d: d[1],reverse = True)
+        A_P.append(AP( result, data.label))
+    m_a_p = sum(A_P) /  length
+    return m_a_p
+
+#eval(tree1,testData)
+
+
+MIN_SIZE = 5000
+
+def main_tree():
+    mv_instructions = ["mvpar", "mvLeftSibl", "mvRightSibl", "mvFirstChild", "mvLastChild", "mvPrevDFS", "mvNextDFS",
+                       "mvPrevLeft", "mvNextLeft", "mvPrevNodeValue", "mvPrevNodeType","mvPrevNodeContext"]
+    write_instruction = [["wrVal"],["wrType"]]
+
+
+    #1. data load and shuffle
+    value_vocab,type_vocab,quer_data = gene_queries()
+
+    now = time.time()
+    print("1 data load",now- start_time)
+    random.shuffle(quer_data) #shuffle
+
+    split_pos = int(len(quer_data) * 0.7)
+    training_data = quer_data[:split_pos]
+    test_data = quer_data[split_pos:]
+
+
+    # 2. make instructions set and shuffle
+    feature_num = 5
+    print("feature_num",feature_num)
+    instructions = gene_feature(mv_instructions,write_instruction,feature_num)
+    now = time.time()
+    print("2 get feature",now- start_time)
+    #random.shuffle(instructions)
+    print("data set size",len(training_data),len(test_data))
+
+    # 3. creat tree
+    my_tree = create_tree(training_data,instructions)
+    now = time.time()
+    print("3 get myTree",now- start_time)
+    print("my_tree",my_tree)
+
+    # 4 . save tree
+    print(my_tree)
+    import json
+    import pickle
+    with open('myTree2.pickle', 'wb') as f1:
+        pickle.dump(my_tree, f1, protocol=pickle.HIGHEST_PROTOCOL)
+    #with open('myTree.json', 'wb') as outf:
+        #json.dump(my_tree, outf, ensure_ascii=False)
+    now = time.time()
+    print("4 save tree", now - start_time)
+
+    #5. get probabilistic model
+    model_tree = traverse2model(my_tree )
+    now = time.time()
+    print("5 model", now - start_time)
+
+    with open('model.pickle', 'wb') as f1:
+        pickle.dump( model_tree, f1, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+    #6 evaluate
+    MAP = eval(model_tree,test_data)
+    print("MAP:",MAP)
+    now = time.time()
+    m, s = divmod((now - start_time), 60)
+    h, m = divmod(m, 60)
+    print("number of classifier",length[0])
+    print("6 evaluate time spend%02d:%02d:%02d: " % (h, m, s))
+
+
+if __name__ == '__main__':
+    main_tree()
+
+
+
---
+++ DT_4/gene_queries_occur1.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+"""
+@version: python3.7
+@author: v-enshi
+@license: Apache Licence 
+@contact: 123@qq.com
+@site: 
+@software: PyCharm
+@file: gene_queries_occur1.py
+@time: 2019/5/8 12:41
+"""
+
+import json
+import copy
+from Tgen1 import TGEN
+import time
+
+
+def gene_queries():
+
+    use_gpu = False
+    use_gpu = True
+
+    if use_gpu:
+
+        max_vocab_size = 50000
+        min_snippet_len = 50
+        type_fre = 130
+        CONTEXT_WINDOW = 50
+    else:
+
+        max_vocab_size = 418
+        min_snippet_len = 3
+        type_fre = 1
+        CONTEXT_WINDOW = 30
+
+
+
+    ##1. data loading {"type":xxx, "children":XXX} or {"type":xxx, "value":XXX}
+    def data_loading(filepath):
+        data = []
+        with open(filepath, 'r') as load_f:
+            data1 = load_f.readlines()
+        for i in range(len(data1)):
+            content = json.loads(data1[i])
+            #data = data + [content[:i+min_snippet_len] for i in range(len(content)-min_snippet_len-1)]
+            data.append(content)
+            if i > 2000:
+                break
+
+
+        return data
+
+
+    # taining data
+    if use_gpu:
+        training_path = r"../data/python/python100k_train.json"
+
+    else:
+        str1 = r"D:\v-enshi\Language model\suggestion\Code Completion with Neural Attention and Pointer Networks"
+        training_path = str1 + r"\data\python\f50_.json"
+
+    training_data = data_loading(training_path)
+
+    '''
+    print(20*"*")
+    print(training_data[:5])
+    print(20*"*")
+    '''
+
+    ##2. add parent node
+    def add_parent(dataList):#[ [{},{}], [] ]
+        data_par = copy.deepcopy(dataList)
+        for data in data_par: #[{},{}]
+
+            for i in range(len(data)): #{ "children":[]}
+
+                if "children" in data[i]:
+                    child_list = data[i]["children"]       # []
+                    for index in child_list:
+
+                        data[index]["parent"] = i
+
+        return data_par
+
+    add_par_data = add_parent(training_data)
+    '''
+    print(20*"*")
+    print(training_data[:5])
+    print(add_par_data[0][:5])
+    print(20*"*")
+    '''
+
+    ## 3. build vocabulary
+    def build_vocab(data):
+        type_to_ix = {}
+        word_to_ix = {}
+        for i in range(len(data)):
+            for item in data[i]:
+                if "type" in item.keys():
+                    if item["type"] in type_to_ix:
+                        type_to_ix[item["type"]] = type_to_ix[item["type"]] + 1
+                    else:
+                        type_to_ix[item["type"]] = 1
+
+                if "value" in item.keys():
+                    if item["value"] in word_to_ix:
+                        word_to_ix[item["value"]] = word_to_ix[item["value"]] + 1
+                    else:
+                        word_to_ix[item["value"]] = 1
+
+        # 1k 10k  50k vocabulary
+        L = sorted(word_to_ix.items(), key=lambda item: item[1], reverse=True)
+        value_to_ix = {"EOF": 0,"UNK":1,"EMPTY":3}
+        for i in range(len(L)):
+            value_to_ix[L[i][0]] = len(value_to_ix)
+
+        L1 = sorted(type_to_ix.items(), key=lambda item: item[1], reverse=True)
+        type_to_ix = {"EOF": 0, "UNK": 1}
+        type_comm = {"EOF": 0, "UNK": 1}
+        type_comm = []
+        for i in range(len(L1)):
+            type_to_ix[L1[i][0]] = len(type_to_ix)
+
+        return type_to_ix,value_to_ix
+
+    type_vocab,value_vocab = build_vocab(training_data)
+
+    ## 4 make queries
+    def make_queries(dataList):
+        queries = []
+        for data in dataList: # one AST
+            length = len(data)
+            if length  < min_snippet_len or length > 2000: #  quit too small sample
+                continue
+
+            for i in range(min_snippet_len,length ): # make queries
+                if "value" not in data[i]: #fine terminal
+                    continue
+                # find same terminal  in the context
+                for j in range(i - 1, max(i - CONTEXT_WINDOW - 1,-1),-1):  #
+                    if "value" in data[j] and data[j]["value"] == data[i]["value"]:
+                        Tree = data[:i]
+
+                        node = copy.deepcopy(data[i])
+                        label = data[i].get("value")
+                        node["value"] = "need_to_predict"
+                        #print(data[:i+1])
+
+                        Tree.append(node)
+                        queries.append(TGEN(Tree, node, label))
+                        break
+                        #print(Tree,label)
+                if j == max(i - CONTEXT_WINDOW - 1,-1):  # there is no same node in context
+                    continue
+
+
+        return queries
+
+    quer_data = make_queries(training_data)
+    #print(len(quer_data))
+    '''
+        print(20*"*")
+        print(quer_data[0])
+        print(quer_data)
+    '''
+    return value_vocab,type_vocab,quer_data
+
