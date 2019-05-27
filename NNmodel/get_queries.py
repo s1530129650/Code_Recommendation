9f32b6c50891f1455dc3c50a3dcbd996ba5a627d
Enshengshi <34572118+s1530129650@users.noreply.github.com>
2019-05-27 09:42:26 +0800

    add NNmodel

---
+++ NNmodel/get_queries.py
@@ -0,0 +1,358 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+"""
+@version: python3.7
+@author: v-enshi
+@license: Apache Licence 
+@contact: 123@qq.com
+@site: 
+@software: PyCharm
+@file: get_queries.py
+@time: 2019/5/20 16:38
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import json
+import random
+import numpy as np
+import time
+
+torch.manual_seed(1)
+
+use_gpu = False
+#use_gpu = True
+
+if use_gpu:
+    device = torch.device("cuda")
+    max_vocab_size = 10000
+    CONTEXT_WINDOW = 50
+    Max_Seq_Len = 2000
+    min_snippet_len = 20
+else:
+    device = torch.device("cpu")
+    max_vocab_size = 800
+    CONTEXT_WINDOW = 50
+    Max_Seq_Len = 1000
+    min_snippet_len = 20
+
+time_start = time.time()
+
+
+##1. data loading {"type":xxx, "children":XXX} or {"type":xxx, "value":XXX}
+def data_loading(filepath):
+    data = []
+    with open(filepath, 'r') as load_f:
+        data1 = load_f.readlines()
+    for i in range(len(data1)):
+        content = json.loads(data1[i])
+        data.append(content)
+        if i >5000:
+            break
+
+    return data
+
+
+# taining data
+if use_gpu:
+    training_path = r"../data/python/python100k_train.json"
+else:
+    str = r"D:\v-enshi\Language model\suggestion\Code Completion with Neural Attention and Pointer Networks"
+    training_path = str + r"\data\python\f20_.json"
+
+training_data = data_loading(training_path)
+
+# eval data
+
+if use_gpu:
+    eval_path = r"../data/python/python50k_eval.json"
+else:
+    str = r"D:\v-enshi\Language model\suggestion\Code Completion with Neural Attention and Pointer Networks"
+    eval_path = str + r"\data\python\f10_.json"
+
+eval_data = data_loading(eval_path)
+
+now = time.time()
+print("1.data loading", now - time_start)
+
+'''
+## 2. build vocabulary
+def build_vocab(data1, data2):
+    type_to_ix = {"EOF": 0, "UNK": 1}
+    word_to_ix = {}
+    for i in range(len(data1)):
+        for item in data1[i]:
+            if item["type"] not in type_to_ix:
+                type_to_ix[item["type"]] = len(type_to_ix)
+            if "value" in item.keys():
+                if item["value"] in word_to_ix:
+                    word_to_ix[item["value"]] = word_to_ix[item["value"]] + 1
+                else:
+                    word_to_ix[item["value"]] = 1
+
+    for i in range(len(data2)):
+        for item in data2[i]:
+            if item["type"] not in type_to_ix:
+                type_to_ix[item["type"]] = len(type_to_ix)
+            if "value" in item.keys():
+                if item["value"] in word_to_ix:
+                    word_to_ix[item["value"]] = word_to_ix[item["value"]] + 1
+                else:
+                    word_to_ix[item["value"]] = 1
+
+    # 1k 10k  50k vocabulary
+    L = sorted(word_to_ix.items(), key=lambda item: item[1], reverse=True)
+    print("L len", len(L), L[max_vocab_size][1])
+    value_to_ix = {"EOF": 0, "UNK": 1}
+    for i in range(max_vocab_size):
+        value_to_ix[L[i][0]] = len(value_to_ix)
+    return type_to_ix, value_to_ix
+
+type_vocab, value_vocab = build_vocab(training_data, eval_data)
+print("vocab len: type:", len(type_vocab), "value:", len(value_vocab))
+'''
+
+## 2. build vocabulary
+def build_vocab(data1, data2):
+    type_to_ix = {"EOF": 0, "UNK": 1}
+    word_to_ix = {}
+    for i in range(len(data1)):
+        for item in data1[i]:
+            if item["type"] not in type_to_ix:
+                type_to_ix[item["type"]] = len(type_to_ix)
+            if "value" in item.keys():
+                if item["value"] in word_to_ix:
+                    word_to_ix[item["value"]] = word_to_ix[item["value"]] + 1
+                else:
+                    word_to_ix[item["value"]] = 1
+
+    for i in range(len(data2)):
+        for item in data2[i]:
+            if item["type"] not in type_to_ix:
+                type_to_ix[item["type"]] = len(type_to_ix)
+
+
+    # 1k 10k  50k vocabulary
+    L = sorted(word_to_ix.items(), key=lambda item: item[1], reverse=True)
+    print("L len", len(L), L[max_vocab_size][1])
+    value_to_ix = {"EOF": 0, "UNK": 1}
+    for i in range(max_vocab_size):
+        value_to_ix[L[i][0]] = len(value_to_ix)
+    return type_to_ix, value_to_ix
+
+type_vocab, value_vocab = build_vocab(training_data, eval_data)
+print("vocab len: type:", len(type_vocab), "value:", len(value_vocab))
+
+now = time.time()
+print("2.build vocabulary", now - time_start)
+
+
+# 3. make the queries
+def Queries(Data):
+    data_rd = []
+
+    for data in Data:
+        length = len(data)
+        if length <=  CONTEXT_WINDOW+1  or length > 2000:
+            continue
+        for i in range( CONTEXT_WINDOW , length):  # make queries
+            if "value" not in data[i]:  # fine terminal
+                continue
+
+            query = [data[:i], [data[i]], i]
+            pos = -1
+            flag = False
+            for j in range(i - 1, max(i - CONTEXT_WINDOW - 1, -1), -1):  #
+                #if "value" in data[j]:
+                pos = pos + 1
+                if "value" in data[j] and data[j]["value"] == data[i]["value"]:
+                    query.append(pos)
+                    flag = True
+                    break
+                    # print(Tree,label)
+
+            # no same node in the context
+            if not flag:
+                continue
+
+            # add parents node
+            for j in range(pos - 1, pos - CONTEXT_WINDOW - 2, -1):
+
+                if "children" in data[j].keys() and pos in data[j]["children"]:
+                    query.append(pos - j - 1)
+                    break
+                if j == pos - CONTEXT_WINDOW :
+                    query.append(CONTEXT_WINDOW - 1)
+                    break
+        # query = [context,   predict_node,   position, same_node_position, parent_node_position]
+            data_rd.append(query)
+
+    return data_rd
+
+# shuffle and split
+training_queries = Queries(training_data)
+random.shuffle(training_queries) #shuffle
+print("trn_eval len",len(training_queries))
+trn_queries = training_queries[:200000]
+print("trn_eval size",len(training_queries))
+
+
+from sklearn.model_selection import train_test_split
+train_queries , eval_queries = train_test_split(trn_queries, test_size = 0.3)
+
+#training_queries.sort(key=lambda x: x[2], reverse=True)  # sort,baecause pack_padded_sequence
+train_queries.sort(key=lambda x: x[2], reverse=True)  # sort,baecause pack_padded_sequence
+eval_queries.sort(key=lambda x: x[2], reverse=True)  # sort,baecause pack_padded_sequence
+
+
+test_queries = Queries(eval_data)
+random.shuffle(test_queries)
+test_queries = test_queries[:100000]
+#test_queries =training_queries[200000:300000]
+test_queries.sort(key=lambda x: x[2], reverse=True)  # sort,baecause pack_padded_sequence
+
+# print(training_queries)
+
+now = time.time()
+print("3.make the queries", now - time_start)
+
+
+# 4 text -> index  a sequence
+
+def prepare_sequence(seq, val_to_ix, type_to_ix):  # trans code to idex
+    idxs_ty = []
+    idxs_vl = []
+    UNK = 1
+    for node in seq:
+        value_str = node.get('value', 'UNK')
+        idxs_vl.append(val_to_ix.get(value_str, UNK))
+        idxs_ty.append(type_to_ix[node.get('type', 'UNK')])
+    # print("np.array([idxs_ty, idxs_vl])",np.array([idxs_ty, idxs_vl]))
+    return  torch.tensor([idxs_vl, idxs_ty],dtype = torch.long)
+
+# text -> index corpus
+def textToindex(prepare_sequence,queries,value_vocab,type_vocab):
+    input_value = []
+    input_type= []
+    parent = []
+    target = []
+    #query = [0 context,   1 predict_node,  2 position, 3 same_node_position, 4 parent_node_position]
+    for i in range(len(queries)):
+        sequence = queries[i][0]
+        [input_val, input_ty] = prepare_sequence(sequence, value_vocab, type_vocab)
+        par = torch.tensor(queries[i][4], dtype=torch.long)
+        targ = torch.tensor(queries[i][3], dtype=torch.long)
+
+        input_value.append(input_val)
+        input_type.append(input_ty)
+        parent.append(par)
+        target.append(targ)
+
+    return input_value,input_type,parent,target
+
+# training set -> train set
+
+
+#input_value_train_all,input_type_train_all,parent_train_all,target_train_all = textToindex(prepare_sequence,training_queries,value_vocab,type_vocab)
+
+input_value_train,input_type_train,parent_train,target_train = textToindex(prepare_sequence,train_queries,value_vocab,type_vocab)
+input_value_eval,input_type_eval,parent_eval,target_eval = textToindex(prepare_sequence,eval_queries,value_vocab,type_vocab)
+input_value_test,input_type_test,parent_test,target_test = textToindex(prepare_sequence,test_queries,value_vocab,type_vocab)
+
+now = time.time()
+print("4 text -> index", now - time_start)
+
+
+# 5 padding and save
+import torch.nn.utils.rnn as rnn_utils
+
+import torch.utils.data as data
+
+class MyData(data.Dataset):
+    def __init__(self, data_seq, input_value, input_type, target, parent):
+        self.input_value = input_value
+        self.input_type = input_type
+        self.target = target
+        self.parent = parent
+        self.length = len(self.target)
+        self.data_length = [len(sq) for sq in data_seq]
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        return self.input_type[idx], self.input_value[idx], self.data_length[idx], self.target[idx], self.parent[
+            idx]
+
+import pickle
+import gc
+#all train
+'''
+x_train_all = rnn_utils.pad_sequence(input_value_train_all, batch_first=True)
+y_train_all = rnn_utils.pad_sequence(input_type_train_all, batch_first=True)
+dataAll_train_all = MyData(input_value_train_all, x_train_all, y_train_all, target_train_all, parent_train_all)
+print(20*"#")
+print("train_all_data_length",dataAll_train_all.length)
+
+with open('../data/python/training3_50k.pickle', 'wb') as f3:
+    pickle.dump(dataAll_train_all, f3, protocol=pickle.HIGHEST_PROTOCOL)
+
+del dataAll_train_all
+gc.collect()
+'''
+#train
+
+
+x_train = rnn_utils.pad_sequence(input_value_train, batch_first=True)
+y_train = rnn_utils.pad_sequence(input_type_train, batch_first=True)
+
+dataAll_train = MyData(input_value_train, x_train, y_train, target_train, parent_train)
+
+print("train_data_length",dataAll_train.length)
+
+#print("make mydata time spend",end - start)
+with open('../data/python/train3_50k.pickle', 'wb') as f1:
+    pickle.dump(dataAll_train, f1, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+del dataAll_train
+gc.collect()
+
+# eval
+x_eval = rnn_utils.pad_sequence(input_value_eval, batch_first=True)
+y_eval = rnn_utils.pad_sequence(input_type_eval, batch_first=True)
+dataAll_eval = MyData(input_value_eval, x_eval, y_eval, target_eval, parent_eval)
+print("eval_data_length",dataAll_eval.length)
+
+with open('../data/python/eval3_50k.pickle', 'wb') as f2:
+    pickle.dump(dataAll_eval, f2, protocol=pickle.HIGHEST_PROTOCOL)
+
+del dataAll_eval
+gc.collect()
+
+#test
+x_test = rnn_utils.pad_sequence(input_value_test, batch_first=True)
+y_test = rnn_utils.pad_sequence(input_type_test, batch_first=True)
+dataAll_test = MyData(input_value_test, x_test, y_test, target_test, parent_test)
+print("test_data_length",dataAll_test.length)
+print(20*"#")
+
+with open('../data/python/test3_50k.pickle', 'wb') as f3:
+    pickle.dump(dataAll_test, f3, protocol=pickle.HIGHEST_PROTOCOL)
+
+del dataAll_test
+gc.collect()
+
+now = time.time()
+print("5. padding ", now - time_start)
+
+# 6 save
+np.savez('../data/python/vocabulary_50k.npz', value_vocab=value_vocab, type_vocab=type_vocab)
+now = time.time()
+print("6. save ", now - time_start)
+
+
+
+
---
+++ NNmodel/t_t_1.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+"""
+@version: python3.7
+@author: v-enshi
+@license: Apache Licence 
+@contact: 123@qq.com
+@site: 
+@software: PyCharm
+@file: train_test.py
+@time: 2019/4/26 11:07
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import  pickle
+import random
+import numpy as np
+import time
+torch.manual_seed(1)
+import torch.nn.utils.rnn as rnn_utils
+
+from torch.utils.data import DataLoader
+
+import torch.utils.data as data
+
+from model import main_model
+
+time_start = time.time()
+
+use_gpu = False
+#use_gpu = True
+
+##1. parameters setting
+if use_gpu:
+    device = torch.device("cuda")
+    max_vocab_size = 10000
+    CONTEXT_WINDOW = 50
+    EMBEDDING_value = 256
+    EMBEDDING_type = 128
+    HIDDEN_SIZE = 512
+    BATCH_SIZE = 10
+    TopK = [1,5,10]
+
+else:
+    device = torch.device("cpu")
+    max_vocab_size = 100
+    CONTEXT_WINDOW = 50
+    EMBEDDING_value = 2
+    EMBEDDING_type = 3
+    HIDDEN_SIZE = 5
+    BATCH_SIZE = 10
+    TopK = [1, 5, 10]
+
+# 2.data loading
+class MyData(data.Dataset):
+    def __init__(self,data_seq, input_value, input_type, target, parent):
+        self.input_value = input_value
+        self.input_type = input_type
+        self.target = target
+        self.parent = parent
+        self.length = len(self.target)
+        self.data_length = [len(sq) for sq in data_seq]
+
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        return self.input_type[idx],self.input_value[idx], self.data_length[idx], self.target[idx], self.parent[idx]
+#vocabulary
+
+with np.load(r"../data/python/vocabulary_50k.npz", allow_pickle=True) as arr:
+    value_vocab = arr['value_vocab'].item()
+    type_vocab = arr['type_vocab'].item()
+
+
+with open('../data/python/train3_50k.pickle', 'rb') as f:
+    data_train = pickle.load(f)
+len_train = data_train.length
+print("len_train",len_train)
+data_loader_train = DataLoader(data_train, batch_size= BATCH_SIZE, shuffle=True, drop_last=True)
+
+with open('../data/python/eval3_50k.pickle', 'rb') as f:
+    data_eval = pickle.load(f)
+len_eval = data_eval.length
+print("len_eval",len_eval)
+data_loader_eval = DataLoader(data_eval, batch_size= BATCH_SIZE, shuffle=True, drop_last=True)
+
+
+with open('../data/python/test3_50k.pickle', 'rb') as f:
+    data_test = pickle.load(f)
+len_test = data_test.length
+print("len_test",len_test)
+data_loader_test = DataLoader(data_test, batch_size= BATCH_SIZE, shuffle=True, drop_last=True)
+
+
+now = time.time()
+print("data loading",now-time_start)
+
+
+##3 train
+def train(model,optimizer,data_loader,loss_function):
+    model.train()
+    total_loss = 0
+    for i, data4 in enumerate(data_loader,0):
+        start = time.time()
+        # step1 init
+        optimizer.zero_grad()
+        # data_loder = input_type, input_value,input_length ,target,parent
+        # step 2 prepare the data
+        Z = zip(data4[2], data4[0], data4[1], data4[3],data4[4])
+        Z1 = sorted(list(Z),key = lambda x:x[0],reverse=True )
+        input_len, input1,input2, target, parent = zip(*Z1)
+        input = [torch.cat((input1),0).view(BATCH_SIZE,-1).to(device),torch.cat((input2),0).view(BATCH_SIZE,-1).to(device)]
+        input_len = torch.tensor(list(input_len))
+        target = torch.tensor(list(target),device = device)
+        parent = torch.tensor(list(parent))
+        # step 3 get the scorece
+        yt_point = model(input,input_len, parent)
+        # step 4 train
+        loss = loss_function(yt_point.squeeze(0), target.to(device))
+        loss.backward()
+        optimizer.step()
+        # loss
+        total_loss += loss.item()
+        #topv, topi = yt_point.data.topk(1)
+        #eval_index = topi.squeeze().detach()
+        # print(eval_index)
+        end = time.time()
+        #print(i, "batch time spend", end - start)
+    return total_loss
+
+#4 eval
+def eval(model,eval_data,loss_function):
+    model.eval()
+    #AP
+    def AP(pre, ground_true):
+        Ap = 0
+        for i in range(len(pre)):
+            if pre[i] == ground_true:
+                Ap = Ap + 1 / (i + 1)
+        return Ap
+
+    total_loss = 0
+    A_P = []
+    for i, data4 in enumerate(eval_data,0):
+        start = time.time()
+
+        # step1  prepare the data
+        Z = zip(data4[2], data4[0], data4[1], data4[3],data4[4])
+        Z1 = sorted(list(Z),key = lambda x:x[0],reverse=True )
+        input_len, input1,input2, target, parent = zip(*Z1)
+        input = [torch.cat((input1),0).view(BATCH_SIZE,-1).to(device),torch.cat((input2),0).view(BATCH_SIZE,-1).to(device)]
+        input_len = torch.tensor(list(input_len))
+        target = torch.tensor(list(target),device = device)
+        parent = torch.tensor(list(parent))
+        # step 2 get the scorece
+        yt_point = model(input,input_len, parent)
+        #step 3 MAP
+        output = yt_point.squeeze(0)
+        #topK = [1, 2, 10]
+        _, pred = output.topk(TopK[1], 1)
+        #print("pre",pred)
+       # print("target",target)
+        for batch in range(BATCH_SIZE):
+            A_P.append(AP(pred[batch], target[batch]))
+        #print(A_P)
+        # step 4 loss
+        loss = loss_function(yt_point.squeeze(0), target.to(device))
+
+        # loss
+        total_loss += loss.item()
+        #topv, topi = yt_point.data.topk(1)
+        #eval_index = topi.squeeze().detach()
+        # print(eval_index)
+        end = time.time()
+        #print(i, "batch time spend", end - start)
+    print("A_P",len(A_P),len_eval)
+    m_a_p = sum(A_P) / len(A_P)
+    return total_loss,m_a_p
+
+def main():
+    # 3.model init
+    model = main_model(len(value_vocab), EMBEDDING_value, len(type_vocab), EMBEDDING_type, HIDDEN_SIZE, BATCH_SIZE,
+                       CONTEXT_WINDOW).to(device)
+
+    loss_function = nn.NLLLoss()
+    learning_rate = 0.001
+    #decay = 0.6
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    clip = 5
+    nn.utils.clip_grad_norm_(model.parameters(), clip)
+    losses_train = []
+    losses_eval = []
+    M_A_P = []
+    M_A_P_test = []
+
+    staring_training = time.time()
+    print("starting training ", staring_training - time_start)
+
+    ##  training
+    num_epochs= 100
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
+        print('-' * 10)
+
+        train_loss = train(model,optimizer,data_loader_train,loss_function)
+        val_loss ,m_A_p = eval(model,data_loader_eval,loss_function)
+        _, m_A_p_test = eval(model, data_loader_test, loss_function)
+        now = time.time()
+
+        losses_train.append(train_loss / len_train)
+
+        print("[Epoch:%d] train_loss:%f val_loss:%f test_MAP:%f | time spend: %f"
+              % (epoch + 1, train_loss / len_train,val_loss/len_eval,m_A_p_test,(now - time_start)/60))
+        losses_eval.append(val_loss / len_eval)
+        M_A_P.append(m_A_p)
+        M_A_P_test.append(m_A_p_test)
+        if (epoch+1) % 5 == 0 :
+            print("train loss",losses_train)
+            print("eval loss",losses_eval)
+            print("eval MAP", M_A_P)
+            print("test MAP", M_A_P_test)
+            #torch.save(model.state_dict(),r'params_lstm_attn_50k.pkl')
+    #model.load_state_dict(torch.load(r"./para/"+str(epoch+1)+'params_lstm_attn_50k.pkl'))
+
+    import pandas as pd
+
+    dataframe = pd.DataFrame({'train_loss': losses_train, 'eval_loss': losses_eval,"eval MAP": M_A_P})
+    dataframe.to_csv("test.csv", index=False, sep=',')
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        print("[STOP]", e)
+
+
+
